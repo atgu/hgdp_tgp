3077e95d818dd672fc71c7f8aaf36d17bbe40a2d
LindoNkambule <lindonkambule116@gmail.com>
2021-03-25 02:51:19 +0200

    updated script and added a function for indexing GVCFs

--- gambian_variant_calling.py
+++ gambian_variant_calling.py
@@ -7,6 +7,7 @@ import hailtop.batch as hb
 import hail as hl
 import pandas as pd
 import os
+from typing import List
 
 
 def bytes_to_gb(in_file):
@@ -56,7 +57,7 @@ EOF
 python3 my_script.py
     ''')
     scatter_list.command(f'mv /scatter_intervals {scatter_list.outfiles}')
-    b.write_output(scatter_list.outfiles, f'{out_dir}/scatter_intervals')
+    b.write_output(scatter_list.outfiles, f'{out_dir}/scatter-intervals')
 
     # We return the `scatter_list` Job object that can be used in downstream jobs.
     return scatter_list
@@ -89,13 +90,13 @@ def haplotype_caller_gatk(b: hb.batch.Batch, input_bam: hb.resource.ResourceGrou
       -GQB 10 -GQB 20 -GQB 30 -GQB 40 -GQB 50 -GQB 60 -GQB 70 -GQB 80 -GQB 90 \
       -ERC GVCF')
     variant_calling.command(f'mv {output_file_name} {variant_calling.ofile}')
-    b.write_output(variant_calling.ofile, f'{out_dir}/variant_calling/{bam_filename_no_ext}/{output_file_name}')
+    b.write_output(variant_calling.ofile, f'{out_dir}/variant-calling/{bam_filename_no_ext}/{output_file_name}')
 
     # We return the `variant_calling` Job object that can be used in downstream jobs.
     return variant_calling
 
 
-def merge_vcf(b: hb.batch.Batch, inputs_vcfs_list_file: hb.resource.ResourceFile, output_vcf_name: str = None,
+def merge_vcf(b: hb.batch.Batch, gvcf_list: List = None, output_vcf_name: str = None,
               merge_vcfs_img: str = None, memory: int = 3, out_dir: str = None):
     # Combine multiple VCFs or GVCFs from scattered HaplotypeCaller runs
     docker_image = merge_vcfs_img if merge_vcfs_img else 'us.gcr.io/broad-gotc-prod/genomes-in-the-cloud:2.4.3-1564508330'
@@ -104,16 +105,22 @@ def merge_vcf(b: hb.batch.Batch, inputs_vcfs_list_file: hb.resource.ResourceFile
 
     # disk_size = bytes_to_gb((inputs_vcfs_list * 2.5)) + 10
 
+    merge_vcf_i = ''
+
+    for line in gvcf_list:
+        input_gvcf = b.read_input(line)
+        merge_vcf_i += f'I={input_gvcf} \t'
+
     merge_vcfs = b.new_job(name=output_vcf_name)
     merge_vcfs.image(docker_image)
     merge_vcfs.memory(job_memory)
     # merge_vcfs.memory(disk_size)
     merge_vcfs.command(f'java -Xms2000m -jar /usr/gitc/picard.jar \
       MergeVcfs \
-      INPUT={inputs_vcfs_list_file} \
-      OUTPUT={outname}')
+      {merge_vcf_i} \
+      O={outname}')
     merge_vcfs.command(f'mv {outname} {merge_vcfs.ofile}')
-    b.write_output(merge_vcfs.ofile, f'{out_dir}/merged_gvcf/{output_vcf_name}/{outname}')
+    b.write_output(merge_vcfs.ofile, f'{out_dir}/merged-gvcf/{output_vcf_name}/{outname}')
 
     return merge_vcfs
 
@@ -133,6 +140,8 @@ def validate_vcf(b: hb.batch.Batch, input_vcf: hb.resource.ResourceFile, ref_fas
     validate_gvcf.image(docker_image)
     validate_gvcf.memory(job_memory)
     # validate_gvcf.storage(disk_size)
+    validate_gvcf.command(f'gatk IndexFeatureFile \
+         -F {input_vcf}')
     validate_gvcf.command(f'gatk --java-options -Xms6000m \
       ValidateVariants \
       -V {input_vcf} \
@@ -148,7 +157,7 @@ def validate_vcf(b: hb.batch.Batch, input_vcf: hb.resource.ResourceFile, ref_fas
 def collect_variant_calling_metrics(b: hb.batch.Batch, input_vcf: hb.resource.ResourceGroup,
                                     dbsnp_vcf_file: hb.resource.ResourceGroup, ref_dict: hb.resource.ResourceGroup,
                                     evaluation_int_list: hb.resource.ResourceFile, metrics_basename: str = None,
-                                    memory: int = 3, docker_img: str = None, out_dir: str = None):
+                                    memory: int = 5, docker_img: str = None, out_dir: str = None):
     docker_image = docker_img if docker_img else 'us.gcr.io/broad-gotc-prod/genomes-in-the-cloud:2.4.3-1564508330'
     job_memory = str(memory) + 'Gi'
 
@@ -166,11 +175,34 @@ def collect_variant_calling_metrics(b: hb.batch.Batch, input_vcf: hb.resource.Re
       SEQUENCE_DICTIONARY={ref_dict.dict} \
       TARGET_INTERVALS={evaluation_int_list} \
       GVCF_INPUT=true')
-    collect_vc_metrics.command(f'mv -R {metrics_basename}.variant_calling_* {collect_vc_metrics.ofile}')
-    b.write_output(collect_vc_metrics.ofile,
-                                    f'{out_dir}/variant-calling-metrics/{metrics_basename}.variant_calling_*')
+    # collect_vc_metrics.command(f'ls')
+    collect_vc_metrics.command(f'mv {metrics_basename}.variant_calling_detail_metrics {collect_vc_metrics.detail}')
+    collect_vc_metrics.command(f'mv {metrics_basename}.variant_calling_summary_metrics {collect_vc_metrics.summary}')
+    b.write_output(collect_vc_metrics.detail,
+                   f'{out_dir}/variant-calling-metrics/{metrics_basename}/{metrics_basename}.variant_calling_detail_metrics')
+    b.write_output(collect_vc_metrics.summary,
+                   f'{out_dir}/variant-calling-metrics/{metrics_basename}/{metrics_basename}.variant_calling_summary_metrics')
 
     return collect_vc_metrics
+
+
+def index_gvcf(b: hb.batch.Batch, input_vcf: hb.resource.ResourceFile, output_vcf_ind_name: str = None, memory: int = 3,
+               docker_img: str = None, out_dir: str = None):
+
+    docker_image = docker_img if docker_img else 'us.gcr.io/broad-gatk/gatk:4.0.10.1'
+    job_memory = str(memory) + 'Gi'
+    outname = output_vcf_ind_name + '.g.vcf.gz.tbi'
+
+    index_gvcf_file = b.new_job(name=f'index-{output_vcf_ind_name}')
+    index_gvcf_file.image(docker_image)
+    index_gvcf_file.memory(job_memory)
+    index_gvcf_file.command(f'gatk IndexFeatureFile \
+         -F {input_vcf} \
+         -O {outname}')
+    index_gvcf_file.command(f'mv {outname} {index_gvcf_file.gvcfind}')
+    b.write_output(index_gvcf_file.gvcfind, f'{out_dir}/merged-gvcf/{output_vcf_ind_name}/{outname}')
+
+    return index_gvcf_file
 
 
 if __name__ == '__main__':
@@ -209,11 +241,10 @@ if __name__ == '__main__':
     scatter = hb.Batch(backend=backend,
                        name='scatter-interval-list')
     calling_interval_list = scatter.read_input(args.calling_interval_list)
-    evaluation_interval_list = scatter.read_input(args.evaluation_interval_list)
     scatter_intervals = scatter_interval_list(b=scatter, interval_list_file=calling_interval_list, out_dir=args.out_dir)
     scatter.run()
 
-    interval_files = hl.utils.hadoop_ls(f'{args.out_dir}/scatter_intervals/**')
+    interval_files = hl.utils.hadoop_ls(f'{args.out_dir}/scatter-intervals/**')
 
     var_call = hb.Batch(backend=backend,
                         name='variant-calling')
@@ -226,13 +257,13 @@ if __name__ == '__main__':
     bams_paths = pd.read_csv(args.input_bams, sep='\t', header=None)
     bam_files = []
     for index, row in bams_paths.iterrows():
-        bam_files.append([row[0], row[1]])
+        bam_files.append((row[0], row[1]))
 
-    for bam in bam_files:
-        base_bam = os.path.basename(bam[0])  # get file name without path
+    for bam, bai in bam_files:
+        base_bam = os.path.basename(bam)  # get file name without path
         bam_no_ext = os.path.splitext(base_bam)[0]
-        in_bam = var_call.read_input_group(**{'bam': bam[0],
-                                              'bam.bai': bam[1]})
+        in_bam = var_call.read_input_group(**{'bam': bam,
+                                              'bam.bai': bai})
         for file in interval_files:
             interval_file = var_call.read_input(file['path'])
             # we have to use the first part of the scatter interval file name + bam basename + .g.vcf.gz so that the
@@ -247,37 +278,70 @@ if __name__ == '__main__':
 
     var_call.run()
 
-    for bam in bam_files:
-        base_bam = os.path.basename(bam[0])  # get file name without path
-        bam_no_ext = os.path.splitext(base_bam)[0]
-        gvcfs_to_merge = hl.utils.hadoop_ls(f'{args.out_dir}/variant_calling/{bam_no_ext}/*')
-        with hl.hadoop_open(f'{args.out_dir}/variant_calling/{bam_no_ext}/gvcfs_to_merge.list', 'w') as f:
-            for file in gvcfs_to_merge:
-                f.write(file['path'] + '\n')
-        # hl.hadoop_copy(f'{bam_no_ext}_gvcfs_to_merge.list', f'{args.out_dir}/variant_calling/{bam_no_ext}/')
-
     merge_gvcfs = hb.Batch(backend=backend,
                            name='merge-gvcfs')
 
-    for bam in bam_files:
-        base_bam = os.path.basename(bam[0])  # get file name without path
+    for bam, bai in bam_files:
+        base_bam = os.path.basename(bam)  # get file name without path
         bam_no_ext = os.path.splitext(base_bam)[0]
-        gvcfs_to_merge_file = merge_gvcfs.read_input(f'{args.out_dir}/variant_calling/{bam_no_ext}/gvcfs_to_merge.list')
-        merge_gvcfs_job = merge_vcf(b=merge_gvcfs, inputs_vcfs_list_file=gvcfs_to_merge_file,
+
+        # get gvcf files to merge
+        gvcfs_to_merge = hl.utils.hadoop_ls(f'{args.out_dir}/variant-calling/{bam_no_ext}/*.vcf.gz')
+        gvcfs_list = []
+        for file in gvcfs_to_merge:
+            gvcfs_list.append(file['path'])
+
+        merge_gvcfs_job = merge_vcf(b=merge_gvcfs, gvcf_list=gvcfs_list,
                                     output_vcf_name=bam_no_ext, out_dir=args.out_dir)
 
-    # This is where the pipeline currently fails and throws
-    # Exception in thread "main" java.nio.file.ProviderNotFoundException: Provider "gs" not found
-    # This happens because the file {args.out_dir}/variant_calling/{bam_no_ext}/gvcfs_to_merge.list has a list of gvcs,
-    # one each line, to be merged with a Google Storage path i.e. path starts with a gs:// and picard is not able to
-    # read files from the cloud
-    # TODO: Ask Jackie how we can 'localize' the files
     merge_gvcfs.run()
 
-        # merge_gvcfs = merge_vcf(b=batch, inputs_vcfs_list_file=gvcfs_to_merge_file, output_vcf_name=bam_no_ext)
-        # vcf_validation = validate_vcf(b=batch, input_vcf=merge_gvcfs.ofile, ref_fasta=fasta,dbsnp_vcf_file=dbsnp_vcf,
-        # calling_int_file=calling_interval_list)
-        # collect_var_call_metrics = collect_variant_calling_metrics(b=batch, input_vcf=merge_gvcfs.ofile,
-        # dbsnp_vcf_file=dbsnp_vcf, ref_dict=fasta,
-        # metrics_basename=bam_no_ext,
-        # evaluation_int_list=evaluation_interval_list)
+    validate_index_gvcf = hb.Batch(backend=backend,
+                                   name='validate-index-gvcf')
+
+    fasta_validate = validate_index_gvcf.read_input_group(**{'fasta': args.ref_fasta,
+                                                             'fasta.fai': args.ref_index,
+                                                             'dict': args.ref_dict})
+    dbsnp_vcf_validate = validate_index_gvcf.read_input_group(**{'vcf': args.dbsnp_vcf,
+                                                                 'vcf.idx': args.dbsnp_vcf_ind})
+    calling_interval_list_validate = validate_index_gvcf.read_input(args.calling_interval_list)
+
+    for bam, bai in bam_files:
+        base_bam = os.path.basename(bam)  # get file name without path
+        bam_no_ext = os.path.splitext(base_bam)[0]
+
+        in_vcf = validate_index_gvcf.read_input(f'{args.out_dir}/merged-gvcf/{bam_no_ext}/{bam_no_ext}.g.vcf.gz')
+
+        validate_gvcf_out = validate_vcf(b=validate_index_gvcf, input_vcf=in_vcf, ref_fasta=fasta_validate,
+                                         dbsnp_vcf_file=dbsnp_vcf_validate,
+                                         calling_int_file=calling_interval_list_validate)
+
+        gvcf_index_file = index_gvcf(b=validate_index_gvcf, input_vcf=in_vcf, output_vcf_ind_name=bam_no_ext,
+                                     out_dir=args.out_dir)
+
+    validate_index_gvcf.run()
+
+    collect_vr_metric = hb.Batch(backend=backend,
+                                 name='collect-variant-calling-metrics')
+
+    fasta_collect = collect_vr_metric.read_input_group(**{'fasta': args.ref_fasta,
+                                                          'fasta.fai': args.ref_index,
+                                                          'dict': args.ref_dict})
+    dbsnp_vcf_collect = collect_vr_metric.read_input_group(**{'vcf': args.dbsnp_vcf,
+                                                              'vcf.idx': args.dbsnp_vcf_ind})
+    evaluation_interval_list_collect = collect_vr_metric.read_input(args.evaluation_interval_list)
+
+    for bam, bai in bam_files:
+        base_bam = os.path.basename(bam)  # get file name without path
+        bam_no_ext = os.path.splitext(base_bam)[0]
+
+        vcf_group = collect_vr_metric.read_input_group(**{'vcf': f'{args.out_dir}/merged-gvcf/{bam_no_ext}/{bam_no_ext}.g.vcf.gz',
+                                                          'vcf.tbi': f'{args.out_dir}/merged-gvcf/{bam_no_ext}/{bam_no_ext}.g.vcf.gz.tbi'})
+
+        vc_metrics = collect_variant_calling_metrics(b=collect_vr_metric, input_vcf=vcf_group,
+                                                     dbsnp_vcf_file=dbsnp_vcf_collect, ref_dict=fasta_collect,
+                                                     evaluation_int_list=evaluation_interval_list_collect,
+                                                     metrics_basename=bam_no_ext,
+                                                     out_dir=args.out_dir)
+
+    collect_vr_metric.run()
