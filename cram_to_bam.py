bf29fe90c32eb379df550392a67c7e0176143987
LindoNkambule <lindonkambule116@gmail.com>
2021-03-29 17:51:33 +0200

    calculate job storage using file sizes

--- cram_to_bam.py
+++ cram_to_bam.py
@@ -19,27 +19,35 @@ def bytes_to_gb(file):
     return size_gigs
 
 
-def cram_to_bam(b: hb.batch.Batch, input_cram: hb.resource.ResourceFile, ref_fasta: hb.resource.ResourceGroup,
-                bam_out_name: str = None, memory: int = 15, samtools_image: str = None, out_dir: str = None):
+def cram_to_bam(b: hb.batch.Batch, input_cram_file: str = None, ref_fasta: str = None, ref_dict: str = None,
+                ref_ind: str = None, bam_out_name: str = None, memory: int = 15, samtools_image: str = None,
+                out_dir: str = None):
     docker_image = samtools_image if samtools_image else 'gcr.io/genomics-tools/samtools'
 
     out_bam_name = bam_out_name + '.bam'
 
-    # output_bam_size: float = bytes_to_gb(input_cram) / 0.40
-    # ref_size: float = bytes_to_gb(ref_fasta) + bytes_to_gb(ref_index)
-    # disk_size: int = round(bytes_to_gb(input_cram) + output_bam_size + ref_size) + 25
+    output_bam_size: float = bytes_to_gb(input_cram_file) / 0.40
+    ref_size: float = bytes_to_gb(ref_fasta) + bytes_to_gb(ref_ind)
+    disk_size: int = round(bytes_to_gb(input_cram_file) + output_bam_size + ref_size) + 25
 
     job_memory = str(memory) + 'Gi'
+    job_storage = str(disk_size) + 'Gi'
 
     crams_to_bams = b.new_job(name=out_bam_name)
+    in_cram = b.read_input(input_cram_file)
+    fasta = b.read_input_group(**{'fasta': ref_fasta,
+                                  'fasta.fai': ref_ind,
+                                  'dict': ref_dict})
+
     crams_to_bams.memory(job_memory)
     crams_to_bams.image(docker_image)
-    crams_to_bams.command(f'samtools view -b -T {ref_fasta.fasta} -o {out_bam_name} {input_cram}')
+    crams_to_bams.storage(job_storage)
+    crams_to_bams.command(f'samtools view -b -T {fasta.fasta} -o {out_bam_name} {in_cram}')
     crams_to_bams.command(f'samtools index {out_bam_name}')
     crams_to_bams.command(f'mv {out_bam_name} {crams_to_bams.bamout}')
     crams_to_bams.command(f'mv {out_bam_name}.bai {crams_to_bams.bamind}')
-    b.write_output(crams_to_bams.bamout, f'{out_dir}/Gambian-CRAMS/{out_bam_name}')
-    b.write_output(crams_to_bams.bamind, f'{out_dir}/Gambian-CRAMS/{out_bam_name}.bai')
+    b.write_output(crams_to_bams.bamout, f'{out_dir}/BAMS/{out_bam_name}')
+    b.write_output(crams_to_bams.bamind, f'{out_dir}/BAMS/{out_bam_name}.bai')
 
     return crams_to_bams
 
@@ -67,9 +75,6 @@ if __name__ == '__main__':
                                     bucket=args.bucket)
 
     crams_to_bams_job = hb.Batch(backend=backend, name='cram-to-bam')
-    fasta = crams_to_bams_job.read_input_group(**{'fasta': args.ref_fasta,
-                                                  'fasta.fai': args.ref_index,
-                                                  'dict': args.ref_dict})
 
     crams_paths = pd.read_csv(args.input_crams, sep='\t', header=None)
     crams_paths.columns = ['cram']
@@ -80,8 +85,8 @@ if __name__ == '__main__':
     for cram in cram_files:
         base_cram = os.path.basename(cram)  # get file name without path
         cram_no_ext = os.path.splitext(base_cram)[0]
-        in_cram = crams_to_bams_job.read_input(cram)
-        convert_cram_to_bam = cram_to_bam(b=crams_to_bams_job, input_cram=in_cram, ref_fasta=fasta,
+        convert_cram_to_bam = cram_to_bam(b=crams_to_bams_job, input_cram_file=cram, ref_fasta=args.ref_fasta,
+                                          ref_dict=args.ref_dict, ref_ind=args.ref_index,
                                           bam_out_name=cram_no_ext, out_dir=args.out_dir)
 
     crams_to_bams_job.run()
