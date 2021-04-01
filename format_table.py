dce6388f87d7bda58582b465c48d8d3f8a8401b0
Zan Koenig <ank.koenig@gmail.com>
2021-04-01 14:14:54 -0400

    update table script and gitignore

--- format_table.py
+++ format_table.py
@@ -5,9 +5,17 @@ from tabulate import tabulate
 hl.init()
 
 
+# Prints out the total sample/variant count for an mt
+def printCount(mt):
+    n = mt.count()
+    print('Number of Samples: {}\nNumber of Variants: {}'.format(n[1],n[0]))
 
-# path for sample metadata file
+
+# path for Alicia's sample metadata file
 sample_metadata_path = 'gs://african-seq-data/hgdp_tgp/gnomad_meta_v1.tsv'
+
+# path for Julia's sample metadata file
+jul_metadata_path = ('gs://hgdp_tgp/output/gnomad_v3.1_sample_qc_metadata_hgdp_tgp_subset.ht')
 
 # path for variant qc info
 var_metadata_path = 'gs://gcp-public-data--gnomad/release/3.1/ht/genomes/gnomad.genomes.v3.1.sites.ht'
@@ -15,8 +23,12 @@ var_metadata_path = 'gs://gcp-public-data--gnomad/release/3.1/ht/genomes/gnomad.
 # path for Konrad's densified matrix table
 dense_mt_path = 'gs://hgdp_tgp/output/tgp_hgdp.mt'
 
-# reading in sample qc metadata file
+
+# reading in Alicia's sample metadata file
 sample_meta = hl.import_table(sample_metadata_path, impute=True)
+
+# reading in Julia's sample metadata file
+jul_meta = hl.read_table(jul_metadata_path)
 
 # reading in variant qc information
 var_meta = hl.read_table(var_metadata_path)
@@ -52,60 +64,21 @@ def dict_to_struct(d):
         fields[k] = v
     return hl.struct(**fields)
 
+
 sample_meta = sample_meta.select(**dict_to_struct(d))
 sample_meta = sample_meta.key_by('s')
 
 
-# Correcting the field types from the flattened hail table
-sample_meta = sample_meta.annotate(project_meta=sample_meta.project_meta.annotate(
-    v2_age=hl.float64(sample_meta.project_meta.v2_age),
-    v2_related=hl.bool(sample_meta.project_meta.v2_related),
-    v2_internal=hl.bool(sample_meta.project_meta.v2_internal),
-    v2_neuro=hl.bool(sample_meta.project_meta.v2_neuro),
-    v2_control=hl.bool(sample_meta.project_meta.v2_control),
-    v2_topmed=hl.bool(sample_meta.project_meta.v2_topmed),
-    v2_high_quality=hl.bool(sample_meta.project_meta.v2_high_quality),
-    v2_pcr_free=hl.bool(sample_meta.project_meta.v2_pcr_free),
-    v2_release_2_0_2=hl.bool(sample_meta.project_meta.v2_release_2_0_2),
-    age=hl.int32(sample_meta.project_meta.age),
-    age_alt=hl.int32(sample_meta.project_meta.age_alt)),
-    bam_metrics=sample_meta.bam_metrics.annotate(
-    median_coverage=hl.float64(sample_meta.bam_metrics.median_coverage),
-    median_insert_size=hl.float64(sample_meta.bam_metrics.median_insert_size)),
-    sex_imputation=sample_meta.sex_imputation.annotate(
-    chr20_mean_dp=hl.float32(sample_meta.sex_imputation.chr20_mean_dp),
-    chrX_mean_dp=hl.float32(sample_meta.sex_imputation.chrX_mean_dp),
-    chrY_mean_dp=hl.float32(sample_meta.sex_imputation.chrY_mean_dp),
-    chrX_ploidy=hl.float32(sample_meta.sex_imputation.chrX_ploidy),
-    chrY_ploidy=hl.float32(sample_meta.sex_imputation.chrY_ploidy),
-    impute_sex_stats = sample_meta.sex_imputation.impute_sex_stats.annotate(
-        n_called=hl.int64(sample_meta.sex_imputation.impute_sex_stats.n_called),
-        expected_homs=hl.float64(sample_meta.sex_imputation.impute_sex_stats.expected_homs),
-        observed_homs=hl.int64(sample_meta.sex_imputation.impute_sex_stats.observed_homs))),
-    sample_qc=sample_meta.sample_qc.annotate(
-    n_hom_ref=hl.int64(sample_meta.sample_qc.n_hom_ref),
-    n_het=hl.int64(sample_meta.sample_qc.n_het),
-    n_hom_var=hl.int64(sample_meta.sample_qc.n_hom_var),
-    n_non_ref=hl.int64(sample_meta.sample_qc.n_non_ref),
-    n_singleton=hl.int64(sample_meta.sample_qc.n_singleton),
-    n_snp=hl.int64(sample_meta.sample_qc.n_snp),
-    n_insertion=hl.int64(sample_meta.sample_qc.n_insertion),
-    n_deletion=hl.int64(sample_meta.sample_qc.n_deletion),
-    n_transition=hl.int64(sample_meta.sample_qc.n_transition),
-    n_transversion=hl.int64(sample_meta.sample_qc.n_transversion),
-    n_star=hl.int64(sample_meta.sample_qc.n_star)),
-    population_inference=sample_meta.population_inference.annotate(
-    pca_scores=hl.array(hl.parse_json(sample_meta.population_inference.pca_scores, hl.tarray(hl.tfloat64)))),
-    sample_filters=sample_meta.sample_filters.annotate(
-    hard_filters=hl.set(hl.parse_json(sample_meta.sample_filters.hard_filters, hl.tset(hl.tstr))),
-    qc_metrics_filters=hl.set(hl.parse_json(sample_meta.sample_filters.qc_metrics_filters, hl.tset(hl.tstr)))),
-    relatedness_inference=sample_meta.relatedness_inference.annotate(
-    relationships=hl.set(hl.parse_json(sample_meta.relatedness_inference.relationships, hl.tset(hl.tstr)))))
+new_meta = sample_meta.select(sample_meta.hgdp_tgp_meta, sample_meta.bergstrom)
 
 
+ht = jul_meta.annotate(**new_meta[jul_meta.s])
 
-# Merging the metadata table and dense mt
-ht = sample_meta
+
+ht.write('gs://african-seq-data/hgdp_tgp/hgdp_tgp_sample_metadata.ht')
+
+ht = hl.read_table('gs://african-seq-data/hgdp_tgp/hgdp_tgp_sample_metadata.ht')
+
 
 mt = dense_mt.annotate_cols(**ht[dense_mt.s])
 all_sample_filters = set(ht['sample_filters'])
@@ -114,23 +87,142 @@ bad_sample_filters = {re.sub('fail_', '', x) for x in all_sample_filters if x.st
 # this filters to only variants that passed all gnomad QC or only failed filters in bad_sample_filters
 mt_filt = mt.filter_cols((mt['sample_filters']['qc_metrics_filters'].difference(bad_sample_filters).length() == 0))
 
-
 # Merging the variant QC info with the dataset
 mt = mt.annotate_rows(**var_meta[mt.locus, mt.alleles])
 
 mt.write('gs://african-seq-data/hgdp_tgp/hgdp_tgp_dense_meta_filt.mt')
 
 
-# Creating list with table information
-table = []
-pop_tmp = set(pop)
-for superpop in pop_tmp:
-    subpop_tmp = mt_filt.filter_cols(mt_filt['project_meta'].project_pop == superpop).project_meta.project_subpop.collect()
-    for subpop in set(subpop_tmp):
-        indiv_count = mt_filt.aggregate_cols(hl.agg.counter(mt_filt['project_meta'].project_subpop == subpop))
-        cov = mt_filt.filter_cols(mt_filt)
-        table.append([superpop,subpop,indiv_count[1]])
+# Reading in annotated dataset
+mt = hl.read_matrix_table('gs://african-seq-data/hgdp_tgp/hgdp_tgp_dense_meta_filt.mt')
 
 
+# Removing Outlier Samples
+outlier_set = {'NA20314','NA20299','HG01880','HG01881',
+               'HGDP00130','HGDP00013','HGDP00150',
+               'HGDP00029','HGDP01298','HGDP01303',
+               'LP6005443-DNA_B02','HGDP01300','HG01628',
+               'HG01629','HG01630','HG01694','HG01696',
+               'HGDP00621','HGDP01270','HGDP01271'}
+set_to_remove = hl.literal(outlier_set)
+mt = mt.filter_cols(~set_to_remove.contains(mt['s']))
 
-print(tabulate(table, headers =["Super Population", "Population", "# Individuals"]))
+
+# Generating a VCF
+# changing types to float64 so they can be written out to vcf
+mt_vcf = mt.annotate_rows(info = mt.info.annotate(
+    QUALapprox=hl.float64(mt.info.QUALapprox),
+    SB= mt.info.SB.map(lambda x: hl.float64(x)),
+    MQ=hl.float64(mt.info.MQ),
+    MQRankSum=hl.float64(mt.info.MQRankSum),
+    VarDP=hl.float64(mt.info.VarDP),
+    AS_ReadPosRankSum=hl.float64(mt.info.AS_ReadPosRankSum), 
+    AS_pab_max=hl.float64(mt.info.AS_pab_max), 
+    AS_QD=hl.float64(mt.info.AS_QD), 
+    AS_MQ=hl.float64(mt.info.AS_MQ), 
+    QD=hl.float64(mt.info.QD), 
+    AS_MQRankSum=hl.float64(mt.info.AS_MQRankSum), 
+    FS=hl.float64(mt.info.FS), 
+    AS_FS=hl.float64(mt.info.AS_FS), 
+    ReadPosRankSum=hl.float64(mt.info.ReadPosRankSum), 
+    AS_QUALapprox=hl.float64(mt.info.AS_QUALapprox), 
+    AS_SB_TABLE=mt.info.AS_SB_TABLE.map(lambda x: hl.float64(x)), 
+    AS_VarDP=hl.float64(mt.info.AS_VarDP), 
+    AS_SOR=hl.float64(mt.info.AS_SOR), 
+    SOR=hl.float64(mt.info.SOR), 
+    singleton=hl.bool(mt.info.singleton), 
+    transmitted_singleton=hl.bool(mt.info.transmitted_singleton), 
+    omni=hl.bool(mt.info.omni), 
+    mills=hl.bool(mt.info.omni), 
+    monoallelic=hl.bool(mt.info.monoallelic), 
+    AS_VQSLOD=hl.float64(mt.info.AS_VQSLOD), 
+    InbreedingCoeff=hl.float64(mt.info.InbreedingCoeff)
+))
+
+mt_vcf = mt_vcf.drop('gvcf_info')
+
+hl.export_vcf(mt_vcf, 'gs://african-seq-data/hgdp_tgp/hgdp_tgp_dense_filt.vcf.bgz', parallel='separate_header')
+
+
+# Creating a separate dataset with only the columns needed for table X
+mt = hl.sample_qc(mt, name="new_sample_qc")
+
+mt_table = mt.select_cols(mt.new_sample_qc.n_snp, 
+                          mt.bam_metrics.mean_coverage, 
+                          mt.new_sample_qc.n_singleton, 
+                          mt.hgdp_tgp_meta.Population,
+                          mt.hgdp_tgp_meta.Genetic.region
+                         )
+
+
+# Grabbing only the columns from the matrix table (outputs table of just columns)
+col_table = mt_table.cols()
+
+col_table.checkpoint('gs://african-seq-data/hgdp_tgp/table_x_checkpoint.ht')
+
+
+# Reading in checkpoint col table
+col_table = hl.read_table('gs://african-seq-data/hgdp_tgp/table_x_checkpoint.ht')
+
+
+# Creating three separate tables for each column, and calculating stats for that column grouped by genetic region (pop)
+n_snp = col_table.group_by(
+    col_table.region, col_table.Population).aggregate(
+    n_snp_stats = hl.agg.stats(col_table.n_snp))
+
+n_singleton = col_table.group_by(
+    col_table.region, col_table.Population).aggregate(
+    n_singleton_stats = hl.agg.stats(col_table.n_singleton))
+
+mean_coverage = col_table.group_by(
+    col_table.region, col_table.Population).aggregate(
+    cov_stats = hl.agg.stats(col_table.mean_coverage))
+
+mean_coverage = mean_coverage.flatten()
+
+n_snp = n_snp.flatten()
+
+n_singleton = n_singleton.flatten()
+
+
+mean_coverage = mean_coverage.select('region',
+                                     'Population',
+                                     'cov_stats.n',
+                                     'cov_stats.mean',
+                                     'cov_stats.stdev')
+
+n_snp = n_snp.select('region',
+                     'Population',
+                     'n_snp_stats.n', 
+                     'n_snp_stats.mean',
+                     'n_snp_stats.stdev')
+
+n_singleton = n_singleton.select('region',
+                                 'Population',
+                                 'n_singleton_stats.n',
+                                 'n_singleton_stats.mean',
+                                 'n_singleton_stats.stdev')
+
+
+mean_coverage = mean_coverage.key_by('region','Population')
+
+n_snp = n_snp.key_by('region', 'Population')
+
+n_singleton = n_singleton.key_by('region', 'Population')
+
+
+table = mean_coverage.annotate(n_snp = n_snp[mean_coverage.region, mean_coverage.Population])
+
+
+table = table.annotate(n_singleton = n_singleton[table.region, table.Population])
+
+
+# Flattening out the structs created from annotating the tables
+table = table.flatten()
+
+
+table = table.key_by(table.region, table.Population)
+
+
+table.export('gs://african-seq-data/hgdp_tgp/table_x.tsv', header=True)
+
