53f0a9acea996e8aa05a074a3c4994c0974536ea
Zan Koenig <znfkoenig@gmail.com>
2021-06-08 15:25:47 -0400

    updated gitignore, updated and created separate scripts for qc and formatting table x

--- merge_meta_and_qc.py
+++ merge_meta_and_qc.py
@@ -3,11 +3,14 @@ import re
 
 hl.init()
 
-
-# Prints out the total sample/variant count for an mt
-def printCount(mt):
-    n = mt.count()
-    print('Number of Samples: {}\nNumber of Variants: {}'.format(n[1],n[0]))
+# Takes a list of dicts and converts it to a struct format (works with nested structs too)
+def dict_to_struct(d):
+    fields = {}
+    for k, v in d.items():
+        if isinstance(v, dict):
+            v = dict_to_struct(v)
+        fields[k] = v
+    return hl.struct(**fields)
 
 
 # path for Alicia's sample metadata file
@@ -36,11 +39,14 @@ var_meta = hl.read_table(var_metadata_path)
 dense_mt = hl.read_matrix_table(dense_mt_path)
 
 
-# Unflattening the Sample Metadata File
+# These bits below were written by Tim Poterba to help troubleshoot unflattening a ht with nested structure
+# dict to hold struct names as well as nested field names
 d = {}
 
+# Getting just the row field names 
 row = sample_meta.row_value
 
+# returns a dict with the struct names as keys and their inner field names as values
 for name in row:
     def recur(dict_ref, split_name):
         if (len(split_name) == 1):
@@ -55,30 +61,25 @@ for name in row:
             dict_ref[split_name[0]] = existing
             recur(existing, split_name[1:])
     recur(d, name.split('.'))
-def dict_to_struct(d):
-    fields = {}
-    for k, v in d.items():
-        if isinstance(v, dict):
-            v = dict_to_struct(v)
-        fields[k] = v
-    return hl.struct(**fields)
 
 
+# using the dict created from flattened struct, creating new structs now unflattened
 sample_meta = sample_meta.select(**dict_to_struct(d))
 sample_meta = sample_meta.key_by('s')
 
 
+# grabbing the columns needed from Alicia's metadata
 new_meta = sample_meta.select(sample_meta.hgdp_tgp_meta, sample_meta.bergstrom)
 
-
+# creating a table with Julia's metadata and Alicia's metadata
 ht = jul_meta.annotate(**new_meta[jul_meta.s])
-
 
 ht.write('gs://african-seq-data/hgdp_tgp/hgdp_tgp_sample_metadata.ht')
 
+# reading in table annotated with Alicia and Julia's respective metadata
 ht = hl.read_table('gs://african-seq-data/hgdp_tgp/hgdp_tgp_sample_metadata.ht')
 
-
+# filtering samples to those who should pass QC
 mt = dense_mt.annotate_cols(**ht[dense_mt.s])
 all_sample_filters = set(ht['sample_filters'])
 # bad_sample_filters were removing whole populations (mostly AFR and OCE) that passed all other QC, bad filters
@@ -86,17 +87,17 @@ bad_sample_filters = {re.sub('fail_', '', x) for x in all_sample_filters if x.st
 # this filters to only variants that passed all gnomad QC or only failed filters in bad_sample_filters
 mt_filt = mt.filter_cols((mt['sample_filters']['qc_metrics_filters'].difference(bad_sample_filters).length() == 0))
 
-# Merging the variant QC info with the dataset
-mt = mt.annotate_rows(**var_meta[mt.locus, mt.alleles])
 
-mt.write('gs://african-seq-data/hgdp_tgp/hgdp_tgp_dense_meta_filt.mt')
+# annotating partially filtered dataset with variant metadata
+mt_filt = mt_filt.annotate_rows(**var_meta[mt_filt.locus, mt_filt.alleles])
 
 
-# Reading in annotated dataset
+mt_filt.write('gs://african-seq-data/hgdp_tgp/hgdp_tgp_dense_meta_filt.mt')
+# Reading in the annotated & partially filtered dataset
 mt = hl.read_matrix_table('gs://african-seq-data/hgdp_tgp/hgdp_tgp_dense_meta_filt.mt')
 
 
-# Removing Outlier Samples
+# This is a set of outliers found by Mary during PCA analyses as well as one duplicate sample
 outlier_set = {'NA20314','NA20299','HG01880','HG01881',
                'HGDP00130','HGDP00013','HGDP00150',
                'HGDP00029','HGDP01298','HGDP01303',
@@ -105,9 +106,9 @@ outlier_set = {'NA20314','NA20299','HG01880','HG01881',
                'HGDP00621','HGDP01270','HGDP01271'}
 set_to_remove = hl.literal(outlier_set)
 mt = mt.filter_cols(~set_to_remove.contains(mt['s']))
+mt = mt.distinct_by_col()
 
 
-# Generating a VCF
 # changing types to float64 so they can be written out to vcf
 mt_vcf = mt.annotate_rows(info = mt.info.annotate(
     QUALapprox=hl.float64(mt.info.QUALapprox),
@@ -138,90 +139,15 @@ mt_vcf = mt.annotate_rows(info = mt.info.annotate(
     InbreedingCoeff=hl.float64(mt.info.InbreedingCoeff)
 ))
 
+# writing out a vcf version of the dataset for downstream analyses
 mt_vcf = mt_vcf.drop('gvcf_info')
 
-hl.export_vcf(mt_vcf, 'gs://african-seq-data/hgdp_tgp/hgdp_tgp_dense_filt.vcf.bgz', parallel='separate_header')
+hl.export_vcf(mt_vcf, 'gs://african-seq-data/hgdp_tgp/hgdp_tgp_postqc.vcf.bgz', parallel='separate_header')
 
 
-# Creating a separate dataset with only the columns needed for table X
-mt = hl.sample_qc(mt, name="new_sample_qc")
-
-mt_table = mt.select_cols(mt.new_sample_qc.n_snp, 
-                          mt.bam_metrics.mean_coverage, 
-                          mt.new_sample_qc.n_singleton, 
-                          mt.hgdp_tgp_meta.Population,
-                          mt.hgdp_tgp_meta.Genetic.region
-                         )
+# Subsetting the variants in the dataset to only PASS variants (those which passed variant QC)
+mt = mt.filter_rows(hl.len(mt.filters) !=0  ,keep=False)
 
 
-# Grabbing only the columns from the matrix table (outputs table of just columns)
-col_table = mt_table.cols()
-
-col_table.checkpoint('gs://african-seq-data/hgdp_tgp/table_x_checkpoint.ht')
-
-
-# Reading in checkpoint col table
-col_table = hl.read_table('gs://african-seq-data/hgdp_tgp/table_x_checkpoint.ht')
-
-
-# Creating three separate tables for each column, and calculating stats for that column grouped by genetic region (pop)
-n_snp = col_table.group_by(
-    col_table.region, col_table.Population).aggregate(
-    n_snp_stats = hl.agg.stats(col_table.n_snp))
-
-n_singleton = col_table.group_by(
-    col_table.region, col_table.Population).aggregate(
-    n_singleton_stats = hl.agg.stats(col_table.n_singleton))
-
-mean_coverage = col_table.group_by(
-    col_table.region, col_table.Population).aggregate(
-    cov_stats = hl.agg.stats(col_table.mean_coverage))
-
-mean_coverage = mean_coverage.flatten()
-
-n_snp = n_snp.flatten()
-
-n_singleton = n_singleton.flatten()
-
-
-mean_coverage = mean_coverage.select('region',
-                                     'Population',
-                                     'cov_stats.n',
-                                     'cov_stats.mean',
-                                     'cov_stats.stdev')
-
-n_snp = n_snp.select('region',
-                     'Population',
-                     'n_snp_stats.n', 
-                     'n_snp_stats.mean',
-                     'n_snp_stats.stdev')
-
-n_singleton = n_singleton.select('region',
-                                 'Population',
-                                 'n_singleton_stats.n',
-                                 'n_singleton_stats.mean',
-                                 'n_singleton_stats.stdev')
-
-
-mean_coverage = mean_coverage.key_by('region','Population')
-
-n_snp = n_snp.key_by('region', 'Population')
-
-n_singleton = n_singleton.key_by('region', 'Population')
-
-
-table = mean_coverage.annotate(n_snp = n_snp[mean_coverage.region, mean_coverage.Population])
-
-
-table = table.annotate(n_singleton = n_singleton[table.region, table.Population])
-
-
-# Flattening out the structs created from annotating the tables
-table = table.flatten()
-
-
-table = table.key_by(table.region, table.Population)
-
-
-table.export('gs://african-seq-data/hgdp_tgp/table_x.tsv', header=True)
-
+# writing out the postQC dataset with PCA sample outliers removed and subset to PASS variants
+mt.write('gs://african-seq-data/hgdp_tgp/hgdp_tgp_postQC.mt')
